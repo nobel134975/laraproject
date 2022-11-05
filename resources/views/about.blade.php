d3d85426cb578bb765ef8241648c95e48f156b0d
nobel134975 <siddique762611@gmail.com>
2022-11-05 17:14:38 +0600

    manager routes and name routes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,10 +8,36 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
     </head>
-    <body class="antialiased">
+    <body>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="">Laravel Project</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('service') }}">Service</a>
+                  </li>
+
+                </ul>
+
+              </div>
+            </div>
+          </nav>
 <h1>about</h1>
     </body>
 </html>
